8605c478f0644345e7e9ccc174419f5c47d03a94
Mathiol <mathieuba37@gmail.com>
2022-01-09 15:39:38 +0100

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,9 +1,9 @@
 // var video = document.getElementById("zone_video");
-var url = "https://www.googleapis.com/youtube/v3/search?key=AIzaSyDiECp6jiWwUTSDP2M2d_Nwcfwwjgp6j6c&channelId=UC_sdtjo-z0hBcBp9kMbhHpg&part=snippet,id&order=date&maxResults=1";
+var url = "https://www.googleapis.com/youtube/v3/search?key=AIzaSyDiECp6jiWwUTSDP2M2d_Nwcfwwjgp6j6c&channelId=UC_sdtjo-z0hBcBp9kMbhHpg&part=snippet,id&order=date&maxResults=2";
 
 var succes = function(data) {
     var element = document.getElementById("zone-video");
-    var url_video = data.items[0].id.videoId;
+    var url_video = data.items[1].id.videoId;
 
     element.attributes.src.nodeValue = ("https://www.youtube.com/embed/" + url_video.toString());
 };
